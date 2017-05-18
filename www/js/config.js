ed7a776df0da3252ecdb347fceb0d412a35ae914
Marco <marcorampok@gmail.com>
2017-05-18 22:38:06 +0200

    first commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={	
+	'ApiUrl' : "http://justeat.com.my/mobileapp/api",
+	'DialogDefaultTitle' : "JustEat",
+	'pushNotificationSenderid' : "672529705707",
+
+	'APIHasKey' : "71084efac8029fc7c33c8e1c56b1a0db"
+};
